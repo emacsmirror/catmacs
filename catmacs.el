c74d24be56c73150693d09a7602e013777ff0c98
pymaximus <b17flyboy@gmail.com>
2017-07-30 23:21:43 +1000

    adding some docs and defcustom entries

--- catmacs.el
+++ catmacs.el
@@ -1,18 +1,47 @@
-;;; catmacs --- Prototyping simpel CAT interface for Yaesu FT991A
-
-;;;
-;; Author: Frank Singleton
+;;; catmacs --- Prototyping simple CAT interface for Yaesu FT991A
+;;
+;;  Copyright © 2017 Frank Singleton
+;;
+;;; Commentary:
+;;
+;;  Provides an Emacs CAT client for controlling Yaesu FT991A.
+;;
+;;  Under construction... eventually it will support a Major Mode
+;;
+;;
+;;; Custom:
 ;;
 
+(defgroup catmacs nil
+  "CAT client for Yaesu FT-991A."
+  :prefix "catmacs-"
+  :group 'applications)
 
+(defcustom catmacs-serial-port "/dev/tty.SLAB_USBtoUART"
+  "Serial port for CAT communications."
+  :type '(string)
+  :group 'catmacs)
+
+(defcustom catmacs-baud-rate 38400
+  "Baud rate for CAT serial communications."
+  :type 'integer
+  :group 'catmacs)
+
+(defcustom catmacs-accept-timeout 0.1
+  "Sets the timeout for accepting process output.  See `accept-process-output' for details."
+  :type 'float
+  :group 'catmacs)
+
+;;
 ;;; Code:
+;;
 
 (defun catmacs-send-serial (command)
   "Send a COMMAND string over serial port."
   (with-temp-buffer
     (let ((process (make-serial-process
-		    :port "/dev/tty.SLAB_USBtoUART"
-		    :speed 38400
+		    :port catmacs-serial-port
+		    :speed catmacs-baud-rate
                     :name "rig"
                     :flowcontrol 'hw
                     :coding 'no-conversion
@@ -27,12 +56,12 @@
   (message "process = [%s] command = [%s]" process command)
   (with-current-buffer (process-buffer process)
     (process-send-string process command)
-    (accept-process-output process 0.1)
+    (accept-process-output process catmacs-accept-timeout)
     (buffer-string)))
 
-(catmacs-send-serial "FA013344628;")
+(catmacs-send-serial "FA018344628;")
 
-(catmacs-send-serial "FA;")
+;;(catmacs-send-serial "FA;")
 
 (provide 'catmacs)
 ;;; catmacs.el ends here
